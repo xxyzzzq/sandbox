2db7a66f33cf3d5e9363afd301493d2585e10e31
Zhiqiang Zhang <xxyzzzq@gmail.com>
2017-03-27 10:35:27 +0100

    Update service-worker.js

--- manifest/service-worker.js
+++ manifest/service-worker.js
@@ -5,3 +5,12 @@ self.addEventListener('install', function(e) {
 self.addEventListener('activate', function(e) {
   console.log("Activating");
 });
+
+self.addEventListener('fetch', function(e) {
+  console.log("Fetching");
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
